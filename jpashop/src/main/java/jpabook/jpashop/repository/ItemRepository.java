869eebc797b24f55836b352d5c2420fc9eb3e495
ptoooool <55444828+ptoooool@users.noreply.github.com>
2022-04-17 18:54:21 +0900

    주문기능 완료

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -18,6 +18,8 @@ public class ItemRepository {
             em.persist(item); // 신규등록
         }else{
             em.merge(item); // 이미 존재하기 때문에 update라고보면됨 (진짜update는아님)
+            // merge는 비어있는값이 객체에 있으면 속성자체가 모든 값을 update 하기 때문에
+            // null이 들어가게 될 수있다. 그렇기 때문에 편하긴하지만 실무에서는 쓰이지않는다.
         }
     }
 
--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional //저번에 말한거처럼 위에 readonly를 했으니 save는 따로 다시 transactional하는것
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional //여기서 따로 저장없이 트랜잭션에 의해서 자동으로 커밋이된다.
+    public void updateItem(Long itemId, Book bookParam){
+        Item findItem = itemRepository.findOne(itemId);
+        // findItem으로 불러오고 아래에서 값이 바뀌기 때문에 JPA가 바뀐걸 감지하고
+        // 바뀐걸 update 쿼리를 DB에 날린다. 이게 변경감지를 이용한 update방법
+        findItem.setPrice(bookParam.getPrice());
+        findItem.setName(bookParam.getName());
+        findItem.setStockQuantity(bookParam.getStockQuantity());
     }
 
     public List<Item> findItems(){
